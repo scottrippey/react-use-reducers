294a2a8e215f36394b6283d0add83be8811bad15
scottrippey <scott.william.rippey@gmail.com>
2022-02-17 11:01:29 -0700

    feature(useReducers): simplified implementation

--- src/use-reducers.ts
+++ src/use-reducers.ts
@@ -1,50 +1,38 @@
-import { useState, useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-export function useReducers<
-  TState,
-  TReducers extends Reducers<TState>
->(
+export function useReducers<TState, TReducers extends Reducers<TState>>(
   initial: TState,
   reducers: TReducers,
-): readonly [ TState, TActions<TReducers, TState> ] {
+): readonly [ TState, ActionsFromReducers<TReducers, TState> ] {
   const [ state, setState ] = useState(initial);
 
   const actions = useMemo(() => {
-    return mapObject(reducers, (value: Reducer<TState>) => {
-      return (...args: unknown[]) => setState(s => value(s, ...args));
-    }) as TActions<TReducers, TState>;
+    // Map the "reducers" into "actions":
+    return (Object.keys(reducers) as Array<keyof TReducers>).reduce((actionsResult, key) => {
+      const reducer = reducers[key];
+      const action = (...args: unknown[]) => setState(s => reducer(s, ...args));
+
+      actionsResult[key] = action as ActionFromReducer<typeof reducer, TState>;
+      return actionsResult;
+    }, {} as ActionsFromReducers<TReducers, TState>);
   }, [ reducers ]);
 
   return [ state, actions ] as const;
 }
 
+// A reducer maps from state to state, with optional args:
+export type Reducer<TState> = (prevState: TState, ...args: any[]) => TState
 
-function mapObject<
-  TObj,
-  TMapped
->(
-  obj: TObj,
-  mapper: (value: TObj[keyof TObj], key: keyof TObj) => TMapped,
-) {
-  return (Object.keys(obj) as Array<keyof TObj>).reduce((mapped, key) => {
-    mapped[key] = mapper(obj[key], key);
-    return mapped;
-  }, {} as {
-    [P in keyof TObj]: TMapped
-  });
+// An action signature omits the first 'prevState' argument from the reducer, and returns void
+export type ActionFromReducer<TReducer extends Reducer<TState>, TState> =
+  TReducer extends (prevState: TState, ...args: infer TArgs) => TState
+    ? (...args: TArgs) => void
+    : never
+
+// An object of reducers
+export type Reducers<TState> = Record<string, Reducer<TState>>
+// An object of actions, mapped from an object of reducers:
+export type ActionsFromReducers<TReducers extends Reducers<TState>, TState> = {
+  [P in keyof TReducers]: ActionFromReducer<TReducers[P], TState>;
 }
-
-
-
-
-
-type Reducer<TState> = (prevState: TState, ...args: any[]) => TState
-type Reducers<TState> = Record<string, Reducer<TState>>
-
-type TActions<TReducers extends Reducers<TState>, TState> = {
-  [P in keyof TReducers]: TReducers[P] extends (prevState: TState, ...args: infer TArgs) => TState
-    ? (...args: TArgs) => TState
-    : never;
-}
-
 
